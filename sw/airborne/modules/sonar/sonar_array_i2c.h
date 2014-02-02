84755bcef58099da74a05a96ed8bba4eb5a35969
Sebastian Mai <bastinat0r@bastinat0r.de>
2014-02-02 17:48:05 +0100

    sonar_arry added to airframe, fixing syntax arrays

--- sw/airborne/modules/sonar/sonar_array_i2c.h
+++ sw/airborne/modules/sonar/sonar_array_i2c.h
@@ -24,29 +24,31 @@
  *  @brief driver for the 5 sonar sensors of the ovgu-fink
  */
 
-#ifndef SONAR_I2C_H
-#define SONAR_I2C_H
+#ifndef SONAR_ARRAY_I2C_H
+#define SONAR_ARRAY_I2C_H
 
 #include "std.h"
 #include "mcu_periph/i2c.h"
 
-extern sonar_values_s sonar_values;
 struct sonar_values_s {
 	uint16_t front;
 	uint16_t right;
 	uint16_t back;
 	uint16_t left;
 	uint16_t down;
-}
+};
+extern struct sonar_values_s sonar_values;
 
-extern sonar_data_aviable_s sonar_data_aviable;
-struct sonar_data_aviable_s {
+struct sonar_data_available_s {
 	bool_t front;
 	bool_t right;
 	bool_t back;
 	bool_t left;
 	bool_t down;
-}
+};
+
+void sonar_send_command(uint8_t i2c_addr);
+extern struct sonar_data_aviable_s sonar_data_aviable;
 
 extern void sonar_array_i2c_init(void);
 extern void sonar_array_i2c_periodic(void);
