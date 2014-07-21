bcc9aaaabc58b8e1840b92729d5896c891f3af8c
Andreas Pfohl <andreas@pf0hl.de>
2014-07-21 13:16:33 +0200

    Added blank driver implementation files

---
+++ sw/airborne/modules/rgb_sensor/tcs34725_i2c.c
@@ -0,0 +1,13 @@
+#include "modules/rgb_sensor/tcs34725_i2c.h"
+
+void tcs34725_i2c_init(void) {
+
+}
+
+void tcs34725_read_periodic(void) {
+
+}
+
+void tcs34725_read_event(void) {
+
+}
---
+++ sw/airborne/modules/rgb_sensor/tcs34725_i2c.h
@@ -0,0 +1,3 @@
+extern void tcs34725_i2c_init(void);
+extern void tcs34725_read_periodic(void);
+extern void tcs34725_read_event(void);
