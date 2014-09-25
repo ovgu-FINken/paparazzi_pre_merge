4e1b9db07f9682e64556d033f2c34876c4421b0f
Andreas Pfohl <andreas@pf0hl.de>
2014-09-25 20:09:29 +0200

    Removed down sonar from array

--- sw/airborne/modules/sonar/sonar_array_i2c.c
+++ sw/airborne/modules/sonar/sonar_array_i2c.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2014  Sebastian Mai
- * 
+ * Copyright (C) 2014 Sebastian Mai, Andreas Pfohl
+ *
  * This file is part of paparazzi.
  *
  * paparazzi is free software; you can redistribute it and/or modify
@@ -17,7 +17,6 @@
  * along with paparazzi; see the file COPYING.  If not, write to
  * the Free Software Foundation, 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
- *
  */
 
 #include "modules/sonar/sonar_array_i2c.h"
@@ -29,7 +28,6 @@
 #include "messages.h"
 #include "subsystems/datalink/downlink.h"
 #include "subsystems/datalink/telemetry.h"
-
 
 /** Sonar offset.
  *  Offset value in m (float)
@@ -52,7 +50,7 @@
 
 /** SONAR_ADDR_FRONT
  * 	adress for the front Sensor
- * 	same as RIGHT, LEFT, BACK ... 
+ * 	same as RIGHT, LEFT, BACK ...
  */
 #ifndef SONAR_ADDR_FRONT
 #define SONAR_ADDR_FRONT 0x71
@@ -70,11 +68,7 @@
 #define SONAR_ADDR_LEFT 0x74
 #endif
 
-#ifndef SONAR_ADDR_DOWN
-#define SONAR_ADDR_DOWN 0x75
-#endif
-
-static const char read_order[] =  { SONAR_ADDR_FRONT, SONAR_ADDR_RIGHT, SONAR_ADDR_DOWN, SONAR_ADDR_BACK, SONAR_ADDR_LEFT};
+static const char read_order[] = { SONAR_ADDR_FRONT, SONAR_ADDR_RIGHT, SONAR_ADDR_BACK, SONAR_ADDR_LEFT };
 
 float sonar_distance;
 float sonar_fail_telemetry_pitch;
@@ -91,10 +85,10 @@ struct i2c_transaction sonar_i2c_read_front_trans;
 struct i2c_transaction sonar_i2c_read_right_trans;
 struct i2c_transaction sonar_i2c_read_back_trans;
 struct i2c_transaction sonar_i2c_read_left_trans;
-struct i2c_transaction sonar_i2c_read_down_trans;
 struct i2c_transaction sonar_i2c_write_trans;
 
-void sonar_array_i2c_init(void) {
+void sonar_array_i2c_init()
+{
 	sonar_index = 0;
 	sonar_status = SONAR_STATUS_IDLE;
 
@@ -106,20 +100,16 @@ void sonar_array_i2c_init(void) {
 	sonar_i2c_read_back_trans.buf[1] = 0;
 	sonar_i2c_read_left_trans.buf[0] = 0;
 	sonar_i2c_read_left_trans.buf[1] = 0;
-	sonar_i2c_read_down_trans.buf[0] = 0;
-	sonar_i2c_read_down_trans.buf[1] = 0;
 
 	sonar_data_available.front = FALSE;
 	sonar_data_available.right = FALSE;
 	sonar_data_available.back  = FALSE;
 	sonar_data_available.left  = FALSE;
-	sonar_data_available.down  = FALSE;
 
 	sonar_values.front = 0;
 	sonar_values.right = 0;
 	sonar_values.back  = 0;
 	sonar_values.left  = 0;
-	sonar_values.down  = 0;
 
 	// register telemetry
 	register_periodic_telemetry(DefaultPeriodic, "SONAR_ARRAY", send_sonar_array_telemetry);
@@ -129,8 +119,10 @@ void sonar_array_i2c_init(void) {
 /** sonar_send_command
  *	send take_range_reading command (0x51) to the sonar sensors to trigger the range readin
  */
-void sonar_send_command(uint8_t i2c_addr) {
-	if (sonar_i2c_write_trans.status == I2CTransDone) {
+void sonar_send_command(uint8_t i2c_addr)
+{
+	if (sonar_i2c_write_trans.status == I2CTransDone)
+	{
 		sonar_i2c_write_trans.buf[0] = 0x51;
 		i2c_transmit(&SONAR_I2C_DEV, &sonar_i2c_write_trans, (i2c_addr << 1) & ~0x01, 1); // 7-Bit Adress + write Bit (last bit set to 0)
 	}
@@ -138,11 +130,14 @@ void sonar_send_command(uint8_t i2c_addr) {
 }
 
 
-void query_sensor( int16_t* value, int16_t* old_value, uint8_t i2c_addr, struct i2c_transaction* transaction) {
-	if(transaction->status == I2CTransDone) {
+void query_sensor(int16_t* value, int16_t* old_value, uint8_t i2c_addr, struct i2c_transaction* transaction)
+{
+	if(transaction->status == I2CTransDone)
+	{
 		i2c_receive(&SONAR_I2C_DEV, transaction, (i2c_addr << 1) | 1, 2);
 		int16_t meas = (int16_t) (((uint16_t)(transaction->buf[0]) << 8) | (uint16_t)(transaction->buf[1]));	// recieve mesuarment
-		if(meas > 0) {
+		if(meas > 0)
+		{
 			*old_value = *value;
 			*value = meas;
 		}
@@ -150,21 +145,24 @@ void query_sensor( int16_t* value, int16_t* old_value, uint8_t i2c_addr, struct 
 	transaction->status = I2CTransDone;
 }
 
-void query_all_sensors( void ) {
+void query_all_sensors()
+{
 #ifndef SITL
-	query_sensor(&sonar_values.front,&sonar_values_old.front, SONAR_ADDR_FRONT, &sonar_i2c_read_front_trans);
-	query_sensor(&sonar_values.right,&sonar_values_old.right, SONAR_ADDR_RIGHT, &sonar_i2c_read_right_trans);
-	query_sensor(&sonar_values.back ,&sonar_values_old.back , SONAR_ADDR_BACK, &sonar_i2c_read_back_trans);
-	query_sensor(&sonar_values.left ,&sonar_values_old.left , SONAR_ADDR_LEFT, &sonar_i2c_read_left_trans);
-	query_sensor(&sonar_values.down ,&sonar_values_old.down , SONAR_ADDR_DOWN, &sonar_i2c_read_down_trans);
+	query_sensor(&sonar_values.front, &sonar_values_old.front, SONAR_ADDR_FRONT, &sonar_i2c_read_front_trans);
+	query_sensor(&sonar_values.right, &sonar_values_old.right, SONAR_ADDR_RIGHT, &sonar_i2c_read_right_trans);
+	query_sensor(&sonar_values.back,  &sonar_values_old.back,  SONAR_ADDR_BACK,  &sonar_i2c_read_back_trans);
+	query_sensor(&sonar_values.left,  &sonar_values_old.left,  SONAR_ADDR_LEFT,  &sonar_i2c_read_left_trans);
 #endif
 }
 
 /** Read I2C value to update sonar measurement and request new value
 */
-void sonar_array_i2c_periodic(void) {
+void sonar_array_i2c_periodic()
+{
 #ifndef SITL
-	sonar_index = (sonar_index + 1) % 5;
+	sonar_index++;
+	if(sonar_index > 3)	// biggist index of sonar sensors
+		sonar_index = 0;
 	sonar_send_command(read_order[sonar_index]);
 
 	query_all_sensors();
@@ -172,11 +170,14 @@ void sonar_array_i2c_periodic(void) {
 #warn "SITL not implemented for sonar_array_i2c yet"
 #endif // SITL
 }
-void sonar_array_i2c_event( void ) { 
+
+void sonar_array_i2c_event()
+{
 	// i guess it it not possible to query verything so often
 }
 
-void send_sonar_array_telemetry(void) {
+void send_sonar_array_telemetry()
+{
 	sonar_fail_telemetry_pitch = sonar_failsave_pitch();
 	sonar_fail_telemetry_roll = sonar_failsave_roll();
 	DOWNLINK_SEND_SONAR_ARRAY(DefaultChannel, DefaultDevice,
@@ -184,9 +185,6 @@ void send_sonar_array_telemetry(void) {
 			&sonar_values.right,
 			&sonar_values.back,
 			&sonar_values.left,
-			&sonar_values.down,
 			&sonar_fail_telemetry_pitch,
 			&sonar_fail_telemetry_roll);
 }
-
-
