d7bb70350af6a57f52a322f929c2705be94707a0
finken-gcs <finken@ovgu.de>
2014-11-25 16:40:29 +0100

    working height control
    todo: parameter tuning

--- sw/airborne/modules/finken_model/finken_model_system.c
+++ sw/airborne/modules/finken_model/finken_model_system.c
@@ -28,6 +28,9 @@
 #include "modules/finken_model/finken_model_environment.h"
 
 
+#include "firmwares/rotorcraft/autopilot.h"
+#include <math.h>
+
 // TODO: sane values
 #ifndef FINKEN_SYSTEM_P
 #define FINKEN_SYSTEM_P 0.1
@@ -38,19 +41,23 @@
 #endif
 
 #ifndef FINKEN_THRUST_P
-#define FINKEN_THRUST_P 0.045
+#define FINKEN_THRUST_P /* 0.15 */0.15
 #endif
 
 #ifndef FINKEN_THRUST_I
-#define FINKEN_THRUST_I 0.00
+#define FINKEN_THRUST_I /*  0.05  */0.05
 #endif
 
 #ifndef FINKEN_THRUST_DEFAULT
-#define FINKEN_THRUST_DEFAULT 0.55
+#define FINKEN_THRUST_DEFAULT /* 0.48 */0.48
 #endif
 
 #ifndef FINKEN_SYSTEM_UPDATE_FREQ
 #define FINKEN_SYSTEM_UPDATE_FREQ 30
+#endif
+
+#ifndef FINKEN_VERTICAL_VELOCITY_FACTOR
+#define FINKEN_VERTICAL_VELOCITY_FACTOR 0.02
 #endif
 
 struct system_model_s finken_system_model;
@@ -89,27 +96,14 @@ void update_finken_system_model(void)
   finken_system_model.velocity_y     = finken_sensor_model.velocity_y;
 }
 
-void send_finken_system_model_telemetry(void)
-{
-  DOWNLINK_SEND_FINKEN_SYSTEM_MODEL(
-    DefaultChannel,
-    DefaultDevice,
-    &finken_system_model.distance_z,
-    &finken_system_model.velocity_theta,
-    &finken_system_model.velocity_x,
-    &finken_system_model.velocity_y,
-    &finken_actuators_set_point.alpha,
-    &finken_actuators_set_point.beta,
-    &finken_actuators_set_point.thrust
-  );
-}
-
 /*
  * Use finken_system_set_point to calculate new actuator settings
  */
 float sum_error_x = 0;
 float sum_error_y = 0;
 float sum_error_z = 0;
+float distance_z_old = 0.0;
+
 void update_actuators_set_point(void)
 {
 	float error_x = finken_system_model.velocity_x - finken_system_set_point.velocity_x;
@@ -123,15 +117,43 @@ void update_actuators_set_point(void)
 	finken_actuators_set_point.alpha = (error_y * FINKEN_SYSTEM_P + sum_error_y * FINKEN_SYSTEM_I) * FINKEN_SYSTEM_UPDATE_FREQ;
 
 	float error_z = finken_system_set_point.distance_z - finken_system_model.distance_z;
-	sum_error_z += error_z;
+	if(autopilot_mode == AP_MODE_NAV)
+	{
+		sum_error_z += error_z;
+	}
+	else
+	{
+		sum_error_z = 0;
+	}
 
-	finken_actuators_set_point.thrust = FINKEN_THRUST_DEFAULT + error_z * FINKEN_THRUST_P + sum_error_z * FINKEN_THRUST_I / FINKEN_SYSTEM_UPDATE_FREQ;
+	float velocity_z = (finken_system_model.distance_z - distance_z_old) * FINKEN_SYSTEM_UPDATE_FREQ;
+
+	finken_actuators_set_point.thrust = FINKEN_THRUST_DEFAULT + error_z * FINKEN_THRUST_P;
+	finken_actuators_set_point.thrust += sum_error_z * FINKEN_THRUST_I / FINKEN_SYSTEM_UPDATE_FREQ;
+
+	finken_actuators_set_point.thrust -= FINKEN_VERTICAL_VELOCITY_FACTOR * (velocity_z / (sqrt(1 + velocity_z * velocity_z)));
 
 	if(finken_actuators_set_point.thrust < 0.2)
 		finken_actuators_set_point.thrust = 0.2;
 	else if(finken_actuators_set_point.thrust > 1.0)
 		finken_actuators_set_point.thrust = 1.0;
 
+	distance_z_old = finken_system_model.distance_z;
 
 	// TODO: Theta
+}
+
+void send_finken_system_model_telemetry(void)
+{
+  DOWNLINK_SEND_FINKEN_SYSTEM_MODEL(
+    DefaultChannel,
+    DefaultDevice,
+    &finken_system_model.distance_z,
+    &finken_system_model.velocity_theta,
+    &finken_system_model.velocity_x,
+    &finken_system_model.velocity_y,
+    &finken_actuators_set_point.alpha,
+    &finken_actuators_set_point.beta,
+    &finken_actuators_set_point.thrust
+  );
 }
