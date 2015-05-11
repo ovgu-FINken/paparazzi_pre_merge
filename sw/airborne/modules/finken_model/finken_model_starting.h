ff75fa03f8189fdb307b3b81005d7bfe02af41a2
Fabian Witt <Fabian@SgtBlack.local>
2015-05-11 16:19:12 +0200

    Starting and landing in C
    Starting and landing in C

---
+++ sw/airborne/modules/finken_model/finken_model_starting.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2014 Andreas Pfohl
+ *
+ * This file is part of paparazzi.
+ *
+ * paparazzi is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * paparazzi is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with paparazzi; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ */
+
+/** @file finken_model_starting.h
+ *  @brief module for the system
+ */
+
+#ifndef FINKEN_MODEL_STARTING_H
+#define FINKEN_MODEL_STARTING_H
+
+#include "std.h"
+#include "modules/finken_model/finken_model_actuators.h"
+#include "mcu_periph/link_device.h"
+#include "subsystems/datalink/transport.h"
+#include "subsystems/datalink/telemetry.h"
+
+struct system_model_s {
+  float distance_z;
+  float velocity_theta;
+  float velocity_x;
+  float velocity_y;
+  float thrust;
+};
+
+extern struct system_model_s finken_starting_model;
+extern struct actuators_model_s finken_actuators_set_point;
+
+extern void finken_starting_model_init(void);
+extern void finken_starting_model_periodic(void);
+void update_finken_starting_model(void);
+
+extern void send_finken_starting_model_telemetry(struct transport_tx *trans, struct link_device* link);
+
+#endif
