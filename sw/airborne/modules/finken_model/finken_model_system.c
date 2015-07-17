633c0cced6f3ca91d7344f9f600e5aca62ed533e
Christoph Steup <steup@ivs.cs.ovgu.de>
2015-07-17 12:56:58 +0200

    Anti-Windup for height control + bug fixes

--- sw/airborne/modules/finken_model/finken_model_system.c
+++ sw/airborne/modules/finken_model/finken_model_system.c
@@ -21,45 +21,19 @@
  */
 
 #include "modules/finken_model/finken_model_system.h"
-#include "subsystems/navigation/common_flight_plan.h"
+//#include "subsystems/navigation/common_flight_plan.h"
 
 /* input */
 #include "modules/finken_model/finken_model_sensors.h"
-#include "modules/finken_model/finken_model_environment.h"
+//#include "modules/finken_model/finken_model_environment.h"
 
-#include "subsystems/radio_control.h"
-#include "arch/stm32/subsystems/radio_control/spektrum_arch.h"
+//#include "subsystems/radio_control.h"
+//#include "arch/stm32/subsystems/radio_control/spektrum_arch.h"
 
-#include "finken_model_pid.h"
+//#include "finken_model_pid.h"
 
 #include "firmwares/rotorcraft/autopilot.h"
 #include <math.h>
-
-// TODO: sane values
-#ifndef FINKEN_SYSTEM_P
-#define FINKEN_SYSTEM_P 0.075
-#endif
-
-#ifndef FINKEN_SYSTEM_I
-#define FINKEN_SYSTEM_I 0.00
-#endif
-
-#ifndef FINKEN_THRUST_P
-#define FINKEN_THRUST_P /* 0.15 */0.10
-#endif
-
-#ifndef FINKEN_THRUST_I
-#define FINKEN_THRUST_I /*  0.05  */0.05
-#endif
-
-
-#ifndef FINKEN_SYSTEM_UPDATE_FREQ
-#define FINKEN_SYSTEM_UPDATE_FREQ 30
-#endif
-
-#ifndef FINKEN_VERTICAL_VELOCITY_FACTOR
-#define FINKEN_VERTICAL_VELOCITY_FACTOR 0.04
-#endif
 
 struct system_model_s finken_system_model;
 struct system_model_s finken_system_set_point;
@@ -69,33 +43,29 @@ bool finken_system_model_control_height;
 void update_actuators_set_point(void);
 
 //all init
-float DEG_TO_GRAD_COEFF = 0.01745329251; //math.pi / 180;
+/*float DEG_TO_GRAD_COEFF = 0.01745329251; //math.pi / 180;
 float MAX_PROXY_DIST = 5.00;		//max measurable distance from the sonar
 float MAX_IR_DIST = 2.00;			//max measurable distance from IR sensor
 float TOLERABLE_PROXY_DIST = 0.7;	//minimum treshold - if less, begin collision avoidance
 float FLIGHT_HEIGHT = 1.30;			//the target altitude we will try to maintain at all times
 float MIN_HEIGHT = 0.75;			//the minimum altitude before we can begin using sonars
 
-struct pid_controller zPIDController;
 struct pid_controller xPIDController;
-struct pid_controller yPIDController;
+struct pid_controller yPIDController;*/
 
 
 //this is for creating the different pids and assigning minmax-values to them.
-static void init_pid()
-{
-	zPIDController.p = 1;// zero integral coeff, because limiting the PID output will mess up the integral part
-	zPIDController.d = 1;
-
+/*static void init_pid()
+{*/
 	// PID Controller: try to control roll and pitch directly from the measured distance
-	xPIDController.p = 4.7;
+	/*xPIDController.p = 4.7;
 	xPIDController.d = 6.9;
 	setMinMax(-6, 6, &xPIDController);
 
 	yPIDController.p = 4.7;
 	yPIDController.d = 6.9;
 	setMinMax(-6, 6, &yPIDController);		// Todo or -8, 8? Why different to xPID?
-}
+}*/
 
 void finken_system_model_init(void) {
   finken_system_model.distance_z     = 0.0;
@@ -108,9 +78,9 @@ void finken_system_model_init(void) {
 	finken_actuators_set_point.theta  = 0.0;
 	finken_actuators_set_point.thrust = 0.0;
 
-	finken_system_model_control_height = 0;
+	finken_system_model_control_height = 1;
 
-   init_pid();
+  // init_pid();
 
   register_periodic_telemetry(DefaultPeriodic, "FINKEN_SYSTEM_MODEL", send_finken_system_model_telemetry);
 }
@@ -135,9 +105,8 @@ void update_finken_system_model(void)
 /*
  * Use finken_system_set_point to calculate new actuator settings
  */
-float sum_error_x = 0;
-float sum_error_y = 0;
-float sum_error_z = 0;
+/*float sum_error_x = 0;
+float sum_error_y = 0;*/
 float error_z_k_dec1 = 0.0; 
 float error_z_k_dec2 = 0.0;
 float thrust_k_dec1 = 0.0;
@@ -146,24 +115,24 @@ float thrust_k_dec2 = 0.0;
 //pid_controller for x and y
 //currently, it will only controll the first sonar-parameter (front or left), which is just for testing. should include an integration
 //of front-back and left-right
-static float pid_planar(float sonar_dist_front, float sonar_dist_back, struct pid_controller *pid)
+/*static float pid_planar(float sonar_dist_front, float sonar_dist_back, struct pid_controller *pid)
 {
 	float sonar_dist = sonar_dist_front - sonar_dist_back;	//Todo controller has to be changed for front-back controlling!
-
+*/
 //	test(0.5 - sonar_dist_front, pid);
-
+/*
 	return adjust(sonar_dist, 0.03, pid);		//return pitch or roll
-}
+}*/
 
 void update_actuators_set_point()
 {
 	/* front , back */
-	float error_x =   finken_sensor_model.distance_d_front - finken_sensor_model.distance_d_back;
+	//float error_x =   finken_sensor_model.distance_d_front - finken_sensor_model.distance_d_back;
 	/* left , right */
-	float error_y =   finken_sensor_model.distance_d_left - finken_sensor_model.distance_d_right;
+	//float error_y =   finken_sensor_model.distance_d_left - finken_sensor_model.distance_d_right;
 
-	finken_actuators_set_point.beta = (float)radio_control.values[RADIO_ROLL] / 13000 *10;
-	finken_actuators_set_point.alpha = (float)radio_control.values[RADIO_PITCH] /13000 * 10;
+	//finken_actuators_set_point.beta = (float)radio_control.values[RADIO_ROLL] / 13000 *10;
+	//finken_actuators_set_point.alpha = (float)radio_control.values[RADIO_PITCH] /13000 * 10;
 
 	float error_z_k = finken_system_set_point.distance_z - finken_system_model.distance_z; 
 	
@@ -173,20 +142,21 @@ void update_actuators_set_point()
 		finken_actuators_set_point.beta = pid_planar(finken_sensor_model.distance_d_left, finken_sensor_model.distance_d_right, &yPIDController);	//roll
 	}*/
 
-	if( !finken_system_model_control_height ){
-		error_z_k_dec1 = 0;
-		error_z_k_dec2 = 0;
-		thrust_k_dec1 = 0;
-		thrust_k_dec1 = 0;
-	}
 
-	float thrust_k = 1.6552 * thrust_k_dec1 - 0.6552 * thrust_k_dec2 + 209.0553 * error_z_k - 413.7859 * error_z_k_dec1 + 204.7450 * error_z_k_dec2;
+	float thrust_k = 1.6552f * thrust_k_dec1 - 0.6552f * thrust_k_dec2 + 209.0553f * error_z_k - 413.7859f * error_z_k_dec1 + 204.7450f * error_z_k_dec2;
 	
+	if( !finken_system_model_control_height )
+		thrust_k = 0;
+
 	error_z_k_dec2 = error_z_k_dec1;
 	error_z_k_dec1 = error_z_k;
 	
 	thrust_k_dec2=thrust_k_dec1;
 	thrust_k_dec1=thrust_k;
+
+	if (FINKEN_THRUST_DEFAULT + thrust_k /100 < 0.2 || FINKEN_THRUST_DEFAULT + thrust_k / 100 > 0.8)
+		thrust_k -= 2*(209.0553f - 413.7859f + 204.7450)*(error_z_k+error_z_k_dec1+error_z_k_dec2)/3;
+
 	if(FINKEN_THRUST_DEFAULT + thrust_k / 100 < 0.2){
 		finken_actuators_set_point.thrust = 0.2;
 	}
